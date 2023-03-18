20c5dc01421c62d4a3a56d96ef0d8ce630575e6b
BYT0723 <twang9739@gmail.com>
2023-03-18 23:35:09 +0800

    pref(common): job:new replace jobstart

--- lua/goctl/common.lua
+++ lua/goctl/common.lua
@@ -1,7 +1,7 @@
 local M = {}
 
-local api = vim.api
-local fn = vim.fn
+local api, fn = vim.api, vim.fn
+local job = require("goctl.job")
 
 ---Check goctl binary
 function M.goctl_check()
@@ -21,23 +21,24 @@ end
 
 ---Install goctl
 function M.goctl_install()
-	local cmd = "GOPROXY=https://goproxy.cn/,direct go install github.com/zeromicro/go-zero/tools/goctl@latest"
-	fn.jobstart(cmd)
+	local cmd =
+		{ "GOPROXY=https://goproxy.cn/,direct", "go", "install", "github.com/zeromicro/go-zero/tools/goctl@latest" }
+	job:new(cmd)
 
-	cmd = "!goctl env check -i -f -v"
-	fn.jobstart(cmd)
+	cmd = { "goctl", "env", "check", "-i", "-f", "-v" }
+	job:new(cmd)
 end
 
 ---Upgrade goctl
 function M.goctl_upgrade()
-	local cmd = "goctl upgrade"
-	fn.jobstart(cmd)
+	local cmd = { "goctl", "upgrade" }
+	job:new(cmd)
 end
 
 ---Check goctl environment
 function M.goctl_env()
-	local cmd = "!goctl env"
-	api.nvim_exec(cmd, false)
+	local cmd = { "goctl", "env" }
+	job:new(cmd)
 end
 
 return M
